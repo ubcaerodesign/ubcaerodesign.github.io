68a6251a766dea1ed8ab5364cfcdbb34e43627ad
Radman Yaghoobi <radmany2005@gmail.com>
2025-09-11 16:29:19 -0700

    Why Aerodesign section added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,14 +25,30 @@ export function MissionIcon({title, image, text}: MissionIconProps) {
   );
 }
 
+interface WhyAeroProps {
+  title: string;
+  text: string;
+}
+
+export function WhyAero({title, text}: WhyAeroProps) {
+  return (
+    <div className="flex flex-1 flex-col gap-5 px-5 py-2.5 border-[1.75px] border-aero-light-blue">
+      <div className="relative flex items-center min-h-[90px] min-w-[160px] bottom-10 ml-auto mr-auto -mb-12 px-5 py-2.5 bg-aero-light-blue">
+        <h3 className="w-full font-bold text-xl/5 text-aero-mid-blue text-center">{title}</h3>
+      </div>
+
+      <p className="text-base/5 text-aero-dark-blue">{text}</p>
+    </div>
+  );
+}
+
 export default function Home() {
   const bgStyle = (image: string): React.CSSProperties => ({ // Needs Improvement
     backgroundImage: `url(${image})`,
   });
 
   return (
-    <section>
-
+    <div>
       {/* Homepage Banner */}
       <div className="w-full h-[100svh] max-h-[110vh] 
                       flex items-center
@@ -63,6 +79,16 @@ export default function Home() {
             <PillButton destination="/about" value="ABOUT US" color="border-aero-mid-blue text-aero-mid-blue"/>
           </div>
         </Section>
+
+        <Section>
+          <h2 className="font-titillium font-bold text-4xl/9 text-aero-dark-blue text-center">WHY JOIN US</h2>
+          
+          <div className="w-full flex flex-wrap gap-x-5 gap-y-10 my-10">
+            <WhyAero title="YEARLY DESIGN CYCLE" text="Each year, we create two entirely new aircraft, building upon past experiences and testing new ideas. These yearly design cycles are crucial, providing new members with the chance to gain hands-on experience throughout the entire engineering design process." />
+            <WhyAero title="EVERYONE CONTRIBUTES TO THE FINAL DESIGN" text="New members are recruited on a project-based basis. Everyone joining the team can expect to be assigned anything from broader design work to specific system development from the get-go. The final aircraft is a product of every single member’s efforts." />
+            <WhyAero title="YEARLY DESIGN CYCLE" text="We take pride in fostering a fun and supportive team dynamic. Our roster is packed with internal and external team events and activities designed to help members connect." />
+          </div>
+        </Section>
       </MarginWrapper>
 
       {/* Homepage CTA */}
@@ -80,6 +106,6 @@ export default function Home() {
           </div>
         </MarginWrapper>
       </div>
-    </section>
+    </div>
   );
 }
